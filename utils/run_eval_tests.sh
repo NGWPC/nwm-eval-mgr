d276fe8d21eeaef687468b820c8d12223c1194b0
Yuqiong Liu <yuqiong.liu@U-28HBMJQ9PK1NS.nextgenwaterprediction.com>
2026-08-21 16:39:37 -0400

    updates in responding to review comments

--- utils/run_eval_tests.sh
+++ utils/run_eval_tests.sh
@@ -7,41 +7,26 @@
 #
 #
 
-# create a temporary directory with random prefix to hold the data and venv directories
-TMP_DIR=$(mktemp -d ../temp.XXXX)
+set -euo pipefail
 
-# temporarily move data and venv to avoid docker build context issues
-if [ -d "data" ]; then
-    mv data "$TMP_DIR"/.
-fi
-if [ -d "venv" ]; then
-    mv venv "$TMP_DIR"/.
-fi
-
-# remove dangling images and containers to free up space
-docker system prune
+# uncomment to remove dangling images and containers to free up space if needed
+# docker system prune
 
 # build docker image for nwm_eval
 docker build --build-arg APP_ROOT=/my-app --tag nwm_eval .
 
-# move data and venv back to original location
-if [ -d "$TMP_DIR/data" ]; then
-    mv "$TMP_DIR/data" .
-fi
-if [ -d "$TMP_DIR/venv" ]; then
-    mv "$TMP_DIR/venv" .
-fi
-rm -rf "$TMP_DIR"
-
-# run tests with docker container
+# run tests with docker container 
+# note: the last test is commented out because it requires downloading large amounts of NWM data from GCS, which may take a few hours to complete
 docker run -v $(pwd):$(pwd) -v $HOME:$HOME -w $(pwd) nwm_eval verification configs/config_hindcast.yaml
 docker run -v $(pwd):$(pwd) -v $HOME:$HOME -w $(pwd) nwm_eval verification configs/config_ngencerf.yaml
 docker run -v $(pwd):$(pwd) -v $HOME:$HOME -w $(pwd) nwm_eval verification configs/config_ngensim.yaml
+# docker run -v $(pwd):$(pwd) -v $HOME:$HOME -w $(pwd) nwm_eval verification configs/config_nwm.yaml
 
 # run tests with local venv
 source venv/bin/activate
 python -m nwm_eval configs/config_hindcast.yaml
 python -m nwm_eval configs/config_ngencerf.yaml
 python -m nwm_eval configs/config_ngensim.yaml
+# python -m nwm_eval configs/config_nwm.yaml
 
 echo "All tests passed successfully!"
